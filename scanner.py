73a3d2aadf218583c8f270c75b4793d785a415b8
Siddharth Lambore <siddharth.lambore@gmail.com>
2026-06-29 10:53:57 +0530

    Build initial ARP network scanner

--- scanner.py
+++ scanner.py
@@ -1,7 +1,7 @@
-from scapy.all import ARP, Ether
+from scapy.all import ARP, Ether, srp
 
 print("=" * 40)
-print("      Network Scanner")
+print("        Network Scanner")
 print("=" * 40)
 
 network = input("Enter Network (Example 192.168.1.0/24): ")
@@ -12,8 +12,15 @@ arp_request = ARP(pdst=network)
 # Create Ethernet broadcast frame
 broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
 
-print("\nARP Packet:")
-arp_request.show()
+# Combine Ethernet + ARP
+packet = broadcast / arp_request
 
-print("\nEthernet Frame:")
-broadcast.show()
+# Send packet
+answered, unanswered = srp(packet, timeout=2, verbose=False)
+
+print("\nDevices Found\n")
+
+for sent, received in answered:
+    print(f"IP Address : {received.psrc}")
+    print(f"MAC Address: {received.hwsrc}")
+    print("-" * 35)
